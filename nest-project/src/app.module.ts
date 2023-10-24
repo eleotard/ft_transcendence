750bd76bb20b4d09e3e91ce59f4d92a53e0934f0
Elsie LEOTARD <eleotard@paul-f4Ar7s5.clusters.42paris.fr>
2023-10-24 21:54:42 +0200

    pas de lien brut de la db dans prisma service

--- nest-project/src/app.module.ts
+++ nest-project/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 
+//On importe ces services dans nímporte module de l'app
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule, 
+  ConfigModule.forRoot({
+    isGlobal: true,
+  })],
 })
 export class AppModule {}
